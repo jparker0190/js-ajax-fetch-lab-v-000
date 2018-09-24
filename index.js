cd4393de9692466ce992d18503d4c228893bda0e
Joseph Parker <jparker010190@gmail.com>
2018-09-24 00:12:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ function showResults(json) {
 function forkRepo() {
   const repo = 'learn-co-curriculum/javascript-fetch-lab'
   //use fetch to fork it!
+  fetch(repo).then(res => res.json()).then(json => console.log(json));
 }
 
 function getToken() {
+  return 360651b86551d7d9526bdddabfc185cec438cb66
   //change to your token to run in browser, but set
   //back to '' before committing so all tests pass
   return ''
